719fdbc9e9a3facfa4c7394466b079bc38199f33
Fonctionnaire <thibaut.gruffy@gmail.com>
2017-06-03 14:34:44 +0200

    Ajout du formulaire de contact et du service d'envoi de mail

---
+++ src/AppBundle/Services/ContactMail/ContactMail.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thiba
+ * Date: 02/06/2017
+ * Time: 19:16
+ */
+
+namespace AppBundle\Services\ContactMail;
+
+use Doctrine\ORM\EntityManagerInterface;
+
+
+class ContactMail extends \Twig_Extension
+{
+
+    private $twig;
+    private $mailer;
+    private $em;
+
+    public function __construct(\Swift_Mailer $mailer,\Twig_Environment $twig, EntityManagerInterface $em)
+    {
+        $this->twig = $twig;
+        $this->mailer = $mailer;
+        $this->em = $em;
+    }
+
+    public function sendContactMail($data)
+    {
+        $message = \Swift_Message::newInstance()->setSubject('Contact')
+            ->setFrom('gruffy.thibaut@gmail.com')
+            ->setTo('thibaut.gruffy@gmail.com')
+            ->setBody($this->twig->render(
+                ':Emails:contactMail.html.twig',
+                array(
+                    'name' => $data['name'],
+                    'firstName' => $data['firstName'],
+                    'mail' => $data['mail'],
+                    'subject' => $data['subject'],
+                    'message' => $data['message']
+                )
+            ),
+                'text/html'
+            );
+        $this->mailer->send($message);
+    }
+}
