ee6545421347f20edb2380cac643176efd2163f0
benedicte <ben.guiriec@gmail.com>
2017-06-11 19:26:08 +0200

    suppression use inutilisé

--- src/AppBundle/Controller/Dashboard/DashboardController.php
+++ src/AppBundle/Controller/Dashboard/DashboardController.php
@@ -3,16 +3,10 @@
 namespace AppBundle\Controller\Dashboard;
 
 
-use AppBundle\Form\ObservationFilterType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller as Controller;
-use AppBundle\Entity\Observation;
-use AppBundle\Form\ObservationType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\ResponseHeaderBag;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 
 class DashboardController extends Controller
@@ -26,17 +20,14 @@ class DashboardController extends Controller
      */
     public function viewObservationsAction()
     {
-
-
         $user = $this->getUser();
-
-
         $listObservations = $this->getDoctrine()->getRepository("AppBundle:Observation")->findBy(
             array('user' => $user)
         );
         return $this->render('Dashboard/dashboard.html.twig', array(
             'user' => $user,
-            'listObservations' => $listObservations));
+            'listObservations' => $listObservations
+        ));
     }
 
     /**
@@ -52,6 +43,5 @@ class DashboardController extends Controller
         return $this->render(":Dashboard:profil.html.twig", array(
             'user' => $user
         ));
-
     }
 }
