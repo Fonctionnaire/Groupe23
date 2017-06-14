cc28143862ffbf88f5796c90cdc19691f1e1a083
Fonctionnaire <thibaut.gruffy@gmail.com>
2017-06-14 14:13:53 +0200

    Correction d'une erreur sur l'export. Déplacement de l'export depuis ObsController vers ExportController

---
+++ src/AppBundle/Controller/ExportController.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thiba
+ * Date: 14/06/2017
+ * Time: 14:03
+ */
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use AppBundle\Form\ObservationFilterType;
+
+class ExportController extends Controller
+{
+    /**
+     * @Route("/filter", name="filter")
+     * @Method({"GET", "POST"})
+     *
+     */
+    public function filterAction(Request $request)
+    {
+        $form = $this->createForm(ObservationFilterType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+
+
+            $query = $this->getDoctrine()
+                ->getRepository('AppBundle:Observation')
+                ->createQueryBuilder('o')
+                ->where('o.dateObservation > :debut')
+                ->andWhere('o.dateObservation < :fin');
+
+            if ($data['especeFilter']) {
+                $query->andWhere('o.taxref IN (:taxref)')
+                    ->setParameters(array(
+                        'debut' => $data['debut'],
+                        'fin' => $data['fin'],
+                        'taxref' => $data['taxref']
+                    ));
+            } else {
+                $query->setParameters(array(
+                    'debut' => $data['debut'],
+                    'fin' => $data['fin'],
+                ));
+            }
+
+
+            $observations = $query->getQuery()->getResult();
+
+
+            return $this->exportObservationsAction($observations);
+
+        };
+
+        return $this->render('ExportForm/exportForm.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+    //-------------------------------------------------
+    // Autres fonctions
+    //-------------------------------------------------
+    /**
+     * Fonction d'export Excel des observations
+     * @param $observations
+     * @return \Symfony\Component\HttpFoundation\StreamedResponse
+     */
+    public function exportObservationsAction($observations)
+    {
+        // On appel de service de création de fichier Excel
+        $phpExcelObject = $this->get('phpexcel')->createPHPExcelObject();
+
+        // On définit les propriétés globales du document
+        $phpExcelObject->getProperties()->setCreator("Michel Dujardin")
+            ->setLastModifiedBy("Giulio De Donato")
+            ->setTitle("Liste des observations")
+            ->setSubject("Observations d'oiseaux")
+            ->setDescription("observations d'oiseau recencées par l'assosiation NAO")
+            ->setKeywords("oiseaux nao taxref")
+            ->setCategory("data export");
+
+        // On prépare le titre des colonnes
+        $phpExcelObject->setActiveSheetIndex(0)
+            ->setCellValue('A1', 'id')
+            ->setCellValue('B1', 'Date')
+            ->setCellValue('C1', 'Auteur')
+            ->setCellValue('D1', 'Latitude')
+            ->setCellValue('E1', 'Longitude')
+            ->setCellValue('F1', 'Commentaire')
+            ->setCellValue('G1', 'CDNom')
+            ->setCellValue('H1', 'LbNom')
+            ->setCellValue('I1', 'Nom Vern');
+
+        //ensuite on boucle pour remplir le tableau excel avec nos observations
+        $i = 2;
+        foreach ($observations as $observation) {
+            $phpExcelObject->setActiveSheetIndex(0)
+                ->setCellValue('A' . $i, $observation->getId())
+                ->setCellValue('B' . $i, $observation->getDate())
+                ->setCellValue('C' . $i, $observation->getUser()->getUsername())
+                ->setCellValue('D' . $i, $observation->getLatitude())
+                ->setCellValue('E' . $i, $observation->getLongitude())
+                ->setCellValue('F' . $i, $observation->getComment())
+                ->setCellValue('G' . $i, $observation->getTaxref()->getCdnom())
+                ->setCellValue('H' . $i, $observation->getTaxref()->getLbnom())
+                ->setCellValue('I' . $i, $observation->getTaxref()->getNonvern());
+            $i = $i + 1;
+        }
+
+        // On nomme l'onglet Actif
+        $phpExcelObject->getActiveSheet()->setTitle('Export');
+
+        // On précise quel onglet doit être ouvert lors de l'ouverture du fichier
+        $phpExcelObject->setActiveSheetIndex(0);
+
+        // create the writer
+        $writer = $this->get('phpexcel')->createWriter($phpExcelObject, 'Excel5');
+        // create the response
+        $response = $this->get('phpexcel')->createStreamedResponse($writer);
+        // adding headers
+        $dispositionHeader = $response->headers->makeDisposition(
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+            'liste-observations.xls'
+        );
+        $response->headers->set('Content-Type', 'text/vnd.ms-excel; charset=utf-8');
+        $response->headers->set('Pragma', 'public');
+        $response->headers->set('Cache-Control', 'maxage=1');
+        $response->headers->set('Content-Disposition', $dispositionHeader);
+
+        return $response;
+    }
+}
--- src/AppBundle/Controller/ObservationController.php
+++ src/AppBundle/Controller/ObservationController.php
@@ -3,7 +3,6 @@
 namespace AppBundle\Controller;
 
 
-use AppBundle\Form\ObservationFilterType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller as BaseController;
 use AppBundle\Entity\Observation;
 use AppBundle\Form\ObservationType;
@@ -11,7 +10,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 
 class ObservationController extends BaseController
@@ -88,127 +86,4 @@ class ObservationController extends BaseController
             'observation' => $observation,
         ));
     }
-
-
-    /**
-     * @Route("/filter", name="filter")
-     * @Method({"GET", "POST"})
-     *
-     */
-    public function filterAction(Request $request)
-    {
-        $form = $this->createForm(ObservationFilterType::class);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $data = $form->getData();
-
-
-
-            $query = $this->getDoctrine()
-                ->getRepository('AppBundle:Observation')
-                ->createQueryBuilder('o')
-                ->where('o.dateObservation > :debut')
-                ->andWhere('o.dateObservation < :fin');
-
-            if ($data['especeFilter']) {
-                $query->andWhere('o.taxref IN (:taxref)')
-                    ->setParameters(array(
-                        'debut' => $data['debut'],
-                        'fin' => $data['fin'],
-                        'taxref' => $data['taxref']
-                    ));
-            } else {
-                $query->setParameters(array(
-                    'debut' => $data['debut'],
-                    'fin' => $data['fin'],
-                ));
-            }
-
-
-            $observations = $query->getQuery()->getResult();
-
-
-            return $this->exportObservations($observations);
-
-        };
-
-        return $this->render('ExportForm/exportForm.html.twig', array(
-            'form' => $form->createView(),
-        ));
-    }
-
-    //-------------------------------------------------
-    // Autres fonctions
-    //-------------------------------------------------
-    /**
-     * Fonction d'export Excel des observations
-     * @param $observations
-     * @return \Symfony\Component\HttpFoundation\StreamedResponse
-     */
-    public function exportObservationsAction($observations)
-    {
-        // On appel de service de création de fichier Excel
-        $phpExcelObject = $this->get('phpexcel')->createPHPExcelObject();
-
-        // On définit les propriétés globales du document
-        $phpExcelObject->getProperties()->setCreator("Michel Dujardin")
-            ->setLastModifiedBy("Giulio De Donato")
-            ->setTitle("Liste des observations")
-            ->setSubject("Observations d'oiseaux")
-            ->setDescription("observations d'oiseau recencées par l'assosiation NAO")
-            ->setKeywords("oiseaux nao taxref")
-            ->setCategory("data export");
-
-        // On prépare le titre des colonnes
-        $phpExcelObject->setActiveSheetIndex(0)
-            ->setCellValue('A1', 'id')
-            ->setCellValue('B1', 'Date')
-            ->setCellValue('C1', 'Auteur')
-            ->setCellValue('D1', 'Latitude')
-            ->setCellValue('E1', 'Longitude')
-            ->setCellValue('F1', 'Commentaire')
-            ->setCellValue('G1', 'CDNom')
-            ->setCellValue('H1', 'LbNom')
-            ->setCellValue('I1', 'Nom Vern');
-
-        //ensuite on boucle pour remplir le tableau excel avec nos observations
-        $i = 2;
-        foreach ($observations as $observation) {
-            $phpExcelObject->setActiveSheetIndex(0)
-                ->setCellValue('A' . $i, $observation->getId())
-                ->setCellValue('B' . $i, $observation->getDate())
-                ->setCellValue('C' . $i, $observation->getUser()->getUsername())
-                ->setCellValue('D' . $i, $observation->getLatitude())
-                ->setCellValue('E' . $i, $observation->getLongitude())
-                ->setCellValue('F' . $i, $observation->getComment())
-                ->setCellValue('G' . $i, $observation->getTaxref()->getCdnom())
-                ->setCellValue('H' . $i, $observation->getTaxref()->getLbnom())
-                ->setCellValue('I' . $i, $observation->getTaxref()->getNonvern());
-            $i = $i + 1;
-        }
-
-        // On nomme l'onglet Actif
-        $phpExcelObject->getActiveSheet()->setTitle('Export');
-
-        // On précise quel onglet doit être ouvert lors de l'ouverture du fichier
-        $phpExcelObject->setActiveSheetIndex(0);
-
-        // create the writer
-        $writer = $this->get('phpexcel')->createWriter($phpExcelObject, 'Excel5');
-        // create the response
-        $response = $this->get('phpexcel')->createStreamedResponse($writer);
-        // adding headers
-        $dispositionHeader = $response->headers->makeDisposition(
-            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
-            'liste-observations.xls'
-        );
-        $response->headers->set('Content-Type', 'text/vnd.ms-excel; charset=utf-8');
-        $response->headers->set('Pragma', 'public');
-        $response->headers->set('Cache-Control', 'maxage=1');
-        $response->headers->set('Content-Disposition', $dispositionHeader);
-
-        return $response;
-    }
-
 }
