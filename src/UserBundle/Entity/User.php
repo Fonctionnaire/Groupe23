e2fb462ba82942e98fc755b3e16547040184570a
benedicte <ben.guiriec@gmail.com>
2017-06-14 15:48:17 +0200

    Ajout register profil editprofil et editmdp

--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -7,6 +7,7 @@ use AppBundle\Entity\Observation;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use FOS\UserBundle\Model\User as BaseUser;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * User
@@ -21,8 +22,15 @@ class User extends BaseUser
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-
     protected $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dateRegister", type="datetime")
+     */
+    protected $dateRegister;
+
 
 
     /**
@@ -31,9 +39,70 @@ class User extends BaseUser
     private $observations;
 
 
+    /**
+     * @ORM\Column(name="firstName", type="string", length=255)
+     *
+     * @Assert\NotBlank(message="Veuillez renseigner votre prénom.", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=2,
+     *     max=255,
+     *     minMessage="Le prénom doit comporter plus de 2 caractères.",
+     *     maxMessage="Prénom trop long.",
+     * )
+     */
+    protected $firstName;
+
+    /**
+     * @ORM\Column(name="lastName", type="string", length=255)
+     *
+     * @Assert\NotBlank(message="Veuillez renseigner votre nom.", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=2,
+     *     max=255,
+     *     minMessage="Le nom doit comporter plus de 2 caractères.",
+     *     maxMessage="Nom trop long.",
+     * )
+     */
+    protected $lastName;
+
+
+    /**
+     * @var string
+     * @Assert\NotBlank(message="Veuillez préciser votre code postal.")
+     * @Assert\Length(max="5", min="5", minMessage="Code postal invalide", maxMessage="Code postal invalide")
+     * @Assert\Regex("/[0-9]{2}[0-9]{3}/", message="Veuillez ne pas mettre d'espace entre les chiffres.")
+     *
+     * @ORM\Column(name="codePostal", length=5)
+     */
+    protected $codePostal;
+
+    /**
+     * @ORM\Column(name="town", type="string", length=255)
+     *
+     * @Assert\NotBlank(message="Veuillez renseigner votre ville.")
+     * @Assert\Length(
+     *     min=2,
+     *     max=255,
+     *     minMessage="Le nom de la ville doit comporter plus de 2 caractères.",
+     *     maxMessage="Ce nom est trop long.",
+     * )
+     */
+    protected $town;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(nullable=true, name="birthDate", type="date")
+     * @Assert\Date()
+     */
+    protected $birthDate;
+
+
+
     public function __construct()
     {
         parent::__construct();
+        $this->dateRegister = new \Datetime();
         $this->observations = new ArrayCollection();
 
 
@@ -74,4 +143,151 @@ class User extends BaseUser
     {
         return $this->observations;
     }
+
+    /**
+     * Set dateRegister
+     *
+     * @param \DateTime $dateRegister
+     *
+     * @return User
+     */
+    public function setDateRegister($dateRegister)
+    {
+        $this->dateRegister = $dateRegister;
+
+        return $this;
+    }
+
+    /**
+     * Get dateRegister
+     *
+     * @return \DateTime
+     */
+    public function getDateRegister()
+    {
+        return $this->dateRegister;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return User
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     *
+     * @return User
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set codePostal
+     *
+     * @param string $codePostal
+     *
+     * @return User
+     */
+    public function setCodePostal($codePostal)
+    {
+        $this->codePostal = $codePostal;
+
+        return $this;
+    }
+
+    /**
+     * Get codePostal
+     *
+     * @return string
+     */
+    public function getCodePostal()
+    {
+        return $this->codePostal;
+    }
+
+    /**
+     * Set town
+     *
+     * @param string $town
+     *
+     * @return User
+     */
+    public function setTown($town)
+    {
+        $this->town = $town;
+
+        return $this;
+    }
+
+    /**
+     * Get town
+     *
+     * @return string
+     */
+    public function getTown()
+    {
+        return $this->town;
+    }
+
+    /**
+     * Set birthDate
+     *
+     * @param \DateTime $birthDate
+     *
+     * @return User
+     */
+    public function setBirthDate($birthDate)
+    {
+        $this->birthDate = $birthDate;
+
+        return $this;
+    }
+
+    /**
+     * Get birthDate
+     *
+     * @return \DateTime
+     */
+    public function getBirthDate()
+    {
+        return $this->birthDate;
+    }
+
+
+
 }
