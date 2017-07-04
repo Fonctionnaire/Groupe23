beabc384fbb2284a50ef27ee926aa34cc26455ed
Benoît GRISOT <benoit.grisot@gmail.com>
2017-07-04 22:41:58 +0200

    ajout de règles de sécurisation

--- src/AppBundle/Controller/Taxrefs/ViewObsAdminController.php
+++ src/AppBundle/Controller/Taxrefs/ViewObsAdminController.php
@@ -12,12 +12,15 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
 
 class ViewObsAdminController extends Controller
 {
     /**
      * @return Response
      * @Method("GET")
+     * @Security("has_role('ROLE_ADMIN')")
      * @Route("/admin/observations-validees", name="admin_listTaxrefs")
      */
     public function adminListTaxrefsAction()
