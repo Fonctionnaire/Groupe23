29aea7f56ebac6187bb9f0112a69d50eabbdb891
Fonctionnaire <thibaut.gruffy@gmail.com>
2017-07-05 15:09:13 +0200

    review de l'indexController

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -5,20 +5,15 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller as BaseController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\Response;
 
 class IndexController extends BaseController
 {
     /**
-     * @return Response
      * @Method("GET")
-     * @Route("/", name="index", options = { "utf8": true })
+     * @Route("/", name="index")
      */
     public function accueilAction()
     {
-        $content = $this
-            ->get('templating')
-            ->render('Index/index.html.twig', array('message' => 'Hello Groupe 23 !'));
-        return new Response($content);
+        return $this->render(':Index:index.html.twig');
     }
 }
